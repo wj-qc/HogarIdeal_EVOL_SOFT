e88fc2b6dea912ff7e2229cf90c85dbd147c101e
Carlos Daniel Pasache Sánchez <carlospasachesanchez@gmail.com>
2025-11-30 21:47:43 -0500

    Agregar entidad RecursoInterno

---
+++ CapaEntidad/RecursoInterno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class RecursoInterno
+    {
+        public int IdRecurso { get; set; }
+        public string NombreRecurso { get; set; }
+        public string TipoRecurso { get; set; }
+        public int Cantidad { get; set; }
+        public string Ubicacion { get; set; }
+        public bool Estado { get; set; }
+        public string FechaRegistro { get; set; }
+    }
+}
